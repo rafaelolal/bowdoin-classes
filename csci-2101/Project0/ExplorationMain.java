18f75738a4fff3041af826cf09fb5f68799d0f6d
Rafael Almeida <rafaelpbcp@gmail.com>
2023-09-08 00:42:28 -0400

    added javadoc and output to porject0

--- csci-2101/Project0/ExplorationMain.java
+++ csci-2101/Project0/ExplorationMain.java
@@ -1,11 +1,24 @@
 package Project0;
 
 public class ExplorationMain {
+    /*
+     * Rafael Almeida
+     * Data Structures CSCI 2101 C
+     * 09/08/2023
+     * New Gear: Backpack
+     * New Natural Feature: Mountain
+     * I tried to be as clever as I could about certain aspects of the project
+     * to minimize code used and reasonably improve scalability. I also tried to
+     * only use things taught in class, which in and of itself is a challenge
+     * and required me to think about how I would go about solving problems
+     * with the little we learned.
+     */
     public static void main(String[] args) {
         Person[] people = {
                 new Person("Rafael"),
                 new Person("Deejuanae"),
-                new Person("Dr. Kai")
+                new Person("Dr. Kai"),
+                new Person("No Gear Guy")
         };
 
         people[0].addGear(new Gear[] { new Boat(), new Backpack() });
